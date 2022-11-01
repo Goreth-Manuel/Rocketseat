e7da0017aeb13191904ad49d81a45691e2872359
Goreth Manuel <gorethmanuel0@gmail.com>
2022-11-01 16:44:17 +0000

    Adicionando um modal feito com JS e tailwind :tada:

---
+++ Modal/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./modal/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'white': '#ffffff',
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'tolet': '#5b21b6'
+    },
+  },
+  plugins: [],
+}
